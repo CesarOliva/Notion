893db67b463c5abb81c02305a8e72621ca3df39f
cesar <cesaroliva.work@gmail.com>
2025-07-22 17:54:10 -0600

    Agregando user settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,36 @@
+"use client"
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return (
-        <div>This is a proteced DocumentsPage</div>
+        <div className="flex flex-col justify-center items-center h-full space-y-4">
+            <Image
+                src="/docs.avif"
+                height="300"
+                width="300"
+                alt="Docs"
+                className="dark:hidden"
+            />
+            <Image
+                src="/dark-docs.avif"
+                height="300"
+                width="300"
+                alt="Docs"
+                className="hidden dark:block"
+            />
+
+            <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos;s Notion</h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                    Create a note
+            </Button>
+        </div>
     );
 }
  
